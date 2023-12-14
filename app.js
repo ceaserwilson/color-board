e2943c6f1bdd27452584e455269382378ff9a8b3
Chris-wilson Obidiebube <christianwilsonobi@gmail.com>
2023-12-14 17:59:24 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+const container = document.getElementById("container");
+const colors = [
+  "red",
+  "blue",
+  "green",
+  "yellow",
+  "orange",
+  "gold",
+  "purple",
+  "silver",
+  "white",
+  "pink",
+  "violet",
+];
+const squares = 400;
+
+for (let i = 0; i < squares; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square");
+
+  square.addEventListener("mouseover", () => setColor(square));
+  square.addEventListener("mouseout", () => removeColor(square));
+
+  container.appendChild(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 1px ${color}`;
+}
+function removeColor(element) {
+  element.style.backgroundColor = "#1d1d1d";
+  element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
